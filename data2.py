a1f7f138ed04902037eeb50401cd2d0c6d62e3dc
Liudmila Mihaylichenko <lucy.mihaylichenko@gmail.com>
2023-06-07 14:57:35 +0300

    add 2

---
+++ data2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Загрузка 
+df = pd.read_csv('datasets/data_mod1.csv')
+
+# Заполнение пропущенных зн 
+mean_age = df['Age'].mean()
+
+# Изменение 
+df['Age'].fillna(mean_age, inplace=True)
+
+# Сохранение 
+df.to_csv('datasets/data_mod2.csv', index=False)
